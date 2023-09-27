bdf5ca7c516b981d70d6781ba1dac35ad848ec2f
beatles_without_tea <max.beales@gmail.com>
2023-09-27 18:39:13 +0200

    redesign

--- main.py
+++ main.py
@@ -1,36 +1,68 @@
-import streamlit as st
+import requests
 import pandas as pd
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+from io import StringIO
+import plotly.graph_objs as go
 
-import pydeck as pdk
-import geopandas as gpd
+geojson_url = "https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson"
+geojson_data = requests.get(geojson_url).json()
 
-st.set_page_config(layout="wide")
 
-# Load geospatial data
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+# "Stunting prevalence among children under 5 years of age (% height-for-age <-2 SD), survey-based estimates",
+url = "http://apps.who.int/gho/athena/api/GHO/NUTRITION_ANT_HAZ_NE2?format=csv"
+response = requests.get(url)
 
-wasting = pd.read_csv("cleaned_data/wasting_cleaned.csv")
+# "Overweight prevalence among children under 5 years of age (% weight-for-height >+2 SD), survey-based estimates",
+# "NUTRITION_ANT_WHZ_NE2",
 
-# Merge the geospatial data with your dataset based on ISO codes
-merged = world.set_index('iso_a3').join(wasting.set_index('ISO3Code'))
 
-# Visualize countries based on the National_r column
-view_state = pdk.ViewState(latitude=0, longitude=0, zoom=1)
+# "Wasting prevalence among children under 5 years of age (% weight-for-height <-2 SD), survey-based estimates",
+# NUTRITION_WH_2
 
-# Adjust the color scaling depending on the range of values in National_r ( Point estimates)
-color_expression = "[255, 255 * (1 - properties.National_r / max_value), 0]"
-color_expression = color_expression.replace("max_value", str(merged['National_r'].max()))
 
-country_layer = pdk.Layer(
-    "GeoJsonLayer",
-    data=merged.__geo_interface__,
-    opacity=0.8,
-    filled=True,
-    stroked=False,
-    get_fill_color=color_expression,
-)
+# Convert the string response to a pandas DataFrame
+csv_data = StringIO(response.content.decode('utf-8'))
+df = pd.read_csv(csv_data)
 
-# Display the map in Streamlit
-st.title("National_r Visualization based on ISO codes")
-st.pydeck_chart(pdk.Deck(layers=[country_layer], initial_view_state=view_state))
 
+def keep_most_recent_date(df):
+    df = df.sort_values(by=['COUNTRY', 'YEAR'], ascending=[True, False])
+    df = df.drop_duplicates(subset='COUNTRY', keep='first')
+    return df
+
+# print(df.loc[:,['COUNTRY','YEAR','Numeric']].head(10))
+df = keep_most_recent_date(df).loc[:,['COUNTRY','YEAR','Numeric']]
+
+# # Convert the data to a DataFrame
+# df = pd.DataFrame(data['fact'])
+
+countries = df['COUNTRY']  
+values = df['Numeric']      
+
+
+app = dash.Dash(__name__)
+
+
+
+
+
+fig = px.choropleth_mapbox(df, geojson=geojson_data, locations=countries, color='Numeric',
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 12),
+                           mapbox_style="carto-positron",
+                           zoom=1.5, center = {"lat": 20, "lon": 0},
+                           opacity=0.5,
+                           featureidkey="properties.ISO_A3"
+                          )
+
+
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+app.layout = html.Div([
+    dcc.Graph(figure=fig, style={"width": "100%","height": "80vh"})
+], style={"width": "100%", "padding": "0"})
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
